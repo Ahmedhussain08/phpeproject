d592099843d8e61108f4551d52f2beae343f3552
Ahmedhussain08 <ahmed.hb0806@gmail.com>
2023-03-28 18:22:20 +0500

    updated search.php

--- search.php
+++ search.php
@@ -404,140 +404,13 @@ else{
 	</div>
 
 
-
-	<!-- Slider -->
-	<section class="section-slide">
-		<div class="wrap-slick1">
-			<div class="slick1">
-				<div class="item-slick1" style="background-image: url(images/img1.avif);">
-					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-50 p-b-30 respon5">
-							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Women Collection 2018
-								</span>
-							</div>
-
-							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
-								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									NEW SEASON
-								</h2>
-							</div>
-
-							<div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
-								<a href="index.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="item-slick1" style="background-image: url(images/img2.avif);">
-					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-							<div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Men New-Season
-								</span>
-							</div>
-
-							<div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
-								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									Jackets & Coats
-								</h2>
-							</div>
-
-							<div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
-								<a href="index.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="item-slick1" style="background-image: url(images/img3.avif);">
-					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-							<div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Men Collection 2018
-								</span>
-							</div>
-
-							<div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
-								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									New arrivals
-								</h2>
-							</div>
-
-							<div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
-								<a href="index.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-
 	<!-- Banner -->
 	
 
 
 	<!-- Product -->
 	<section class="bg0 p-t-23 p-b-23">
-		<div class="container">
-			<div class="p-b-5">
-				<h3 class="ltext-103 cl5">
-					Product Overview
-				</h3>
-			</div>
-
-			<div class="flex-w flex-sb-m p-b-52">
-				<div class="flex-w flex-l-m filter-tope-group m-tb-10">
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
-						All Products
-					</button>
-
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".women">
-						Women
-					</button>
-
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".men">
-						Men
-					</button>
-
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".bag">
-						Bag
-					</button>
-
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".shoes">
-						Shoes
-					</button>
-
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".watches">
-						Watches
-					</button>
-				</div>
-
-				<div class="flex-w flex-c-m m-tb-10">
-					<div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
-						<i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
-						<i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-						Filter
-					</div>
-
-					<div class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
-						<i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
-						<i class="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-						Search
-					</div>
-				</div>
+		<div class="container my-5">
 
 				<!-- Search product -->
 				<div class="dis-none panel-search w-full p-t-10 p-b-15">
@@ -747,27 +620,40 @@ else{
 
 		<div class="container">
 			<div class="row text-center">
-				<?php
-				$product = mysqli_query($con, "select * from product where pname like '%$search%'");
-				while ($row = mysqli_fetch_array($product)) {
-				?>
-					<div class="col-md-4 col-lg-3 col-sm-5 ">
-						<div id="card" class="card my-1 bg-white" style="width: 15rem;">
-							<img src="admin/productimages/<?php echo $row[4] ?>" class="card-img-top img-fluid " alt="...">
-							<div class="card-body bg-white">
-								<h5 class="card-title text- fw-bold mb-3"> <?php echo $row[1] ?></h5>
-								<p class="card-text  text-muted mt-2"> <?php echo $row[3] ?> </p>
-								<p href="" class="text-primary mt-2 fw-bold"> RS. <?php echo $row[2] ?> </p>
-								<a href="product-detail.php?productid=<?php echo $row[0] ?>" class=" btn text-white my-3 bg1  hov-btn1"> BUY NOW  </a>
-							</div>
-						</div>
-					</div>
-				<?php } ?>
-				<div class="flex-c-m flex-w w-full p-t-45">
-					<a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
-						Load More
-					</a>
-				</div>
+			<?php
+$product = mysqli_query($con, "SELECT * FROM product WHERE pname LIKE '%$search%'");
+if(mysqli_num_rows($product) == 0) {
+    echo '<h1 class="text-white my-4 p-2 text-center mx-auto">No Products Found.</h1>';
+} else {
+    while ($row = mysqli_fetch_array($product)) {
+?>
+        <div class="col-md-4 col-lg-3 col-sm-5 ">
+            <div id="card" class="card my-1 bg-white" style="width: 15rem;">
+                <img src="admin/productimages/<?php echo $row[4]; ?>" class="card-img-top img-fluid " alt="...">
+                <div class="card-body bg-white">
+                    <h5 class="card-title text- fw-bold mb-3"> <?php echo $row[1]; ?></h5>
+                    <p class="card-text  text-muted mt-2"> <?php echo $row[3]; ?> </p>
+                    <p href="" class="text-primary mt-2 fw-bold"> RS. <?php echo $row[2]; ?> </p>
+                    <a href="product-detail.php?productid=<?php echo $row[0]; ?>" class=" btn text-white my-3 bg1  hov-btn1"> BUY NOW  </a>
+                </div>
+            </div>
+        </div>
+
+		
+<?php 
+    }
+?>
+    <div class="flex-c-m flex-w w-full p-t-45">
+        <a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
+            Load More
+        </a>
+    </div>
+<?php
+}
+?>
+
+
+				
 			</div>
 		</div>
 
